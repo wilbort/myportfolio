253c920dc14aacce5de05578529003004ea216d9
Wilbort Encomenderos <wilbort@gmail.com>
2022-06-08 17:06:29 -0500

    typing animation

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// typing animation
+var typed = new Typed(".typing", {
+    strings: [" ","Web Designer","Web Developer","Graphic Designer"],
+    typeSpeed: 100,
+    BackSpeed: 80,
+    loop: true
+})
