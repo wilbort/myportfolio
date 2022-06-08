96f0f0b676232790ea959f80691ba836309f2c4e
Wilbort Encomenderos <wilbort@gmail.com>
2022-06-08 18:11:57 -0500

    aside active

--- js/script.js
+++ js/script.js
@@ -5,3 +5,21 @@ var typed = new Typed(".typing", {
     BackSpeed: 80,
     loop: true
 })
+
+// aside animation
+const nav = document.querySelector(".nav"),
+    navList = nav.querySelectorAll("li"),
+    totalNavList = navList.length;
+
+    for(let i=0; i<totalNavList;i++)
+    {
+        const a = navList[i].querySelector("a");
+        a.addEventListener("click", function()
+        {
+            for(let j=0; j<totalNavList; j++)
+            {
+                navList[j].querySelector("a").classList.remove("active")
+            }
+            this.classList.add("active")
+        })
+    }
